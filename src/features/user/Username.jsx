3373969b74f69bf708c92563360f1ab9c5af7553
IbadRehman360 <ibadhashim4@gmail.com>
2023-09-21 05:45:07 +0500

    Added some logic, and few pakages, redux-tool/kit and etc

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,10 @@
+import { useSelector } from "react-redux";
+
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Jonas</div>;
+  const user = useSelector((store) => store.user.username)
+
+  if (!user) return
+  return <div className="hidden text-sm font-semibold md:block">{user} </div>;
 }
 
 export default Username;
